d1aedcfc05e9c27250c8ded84d9c505706ea39e1
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-07-25 16:03:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecoveryanytimeCom::Application.config.secret_key_base = '42c197bb24887959403958e89b9b1e230713f73f69349ee195abf437cc2536b39caa6a0e6b57d1b75e8cc7a97591c51a0fc96ee2252d4d460833b8d76729d3a8'
