ae9dd707206b79a8a9dced69a9722ba1d0237c8d
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-07-31 20:23:10 -0400

    privacy_policy link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ RecoveryanytimeCom::Application.routes.draw do
   resources :contacts
   resources :blogs
   resources :meetings
+  match 'privacy_policy' => 'pages#privacy_policy',   as: :privacy_policy, via: :all
   match 'get_help_now' => 'pages#get_help_now',   as: :get_help_now, via: :all
   match 'about_us'     => 'pages#about_us',       as: :about_us, via: :all
   match 'ping'         => 'pages#ping',           as: :ping, via: :all
