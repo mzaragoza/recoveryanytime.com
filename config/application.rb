ea0b5194541f5fdcdfbe98aa4e54e9ec93a77a89
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-08-16 20:01:38 -0400

    fix errrors

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,20 @@ module RecoveryanytimeCom
       end if File.exists?(dev)
     end
 
+
+    initializer 'setup_asset_pipeline', :group => :all do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+          '.html', '.erb', '.haml', # Templates
+          '.png', '.gif', '.jpg', '.jpeg', # Images
+          '.eot', '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+      end)
+    end
+
     config.to_prepare do
       #Devise::Mailer.layout "email" # email.haml or email.erb
     end
