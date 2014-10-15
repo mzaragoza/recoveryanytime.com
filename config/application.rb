4beb030c06f691f03a1a2d7af39637a1f0adf9bf
Aldo Delgado <aldo@osstek.com>
2014-10-15 12:56:24 -0400

    Change application.rb to load vendor assets

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,11 @@ module RecoveryanytimeCom
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    #
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'social-premium', 'assets', 'css')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'social-premium', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'social-premium', 'assets', 'img')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'social-premium', 'assets', 'js')
     config.assets.precompile += %w( .svg .eot .woff .ttf )
     config.autoload_paths += %W(#{config.root}/lib)
 
