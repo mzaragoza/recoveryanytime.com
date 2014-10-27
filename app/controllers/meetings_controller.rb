ee700505e98006d548e7211d63133504ade7489b
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-10-26 23:04:08 -0400

    WIP Create Meeting Missing the create controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -11,7 +11,8 @@ class MeetingsController < PublicController
       end
       meetings.order("meeting_time ASC")
     }
-    expose(:meeting){ Meeting.find(params[:id]) }
+    #expose(:meeting){ Meeting.find(params[:id]) }
+    expose(:meeting, attributes: :meeting_params)
 
     expose(:meetings_count){
       meetings = Meeting.where(:meeting_time => Date.today..Date.today + 7.days)
@@ -25,11 +26,30 @@ class MeetingsController < PublicController
   def check_states
     states_selected = []
     us_states.each do |state|
-      if params[:meeting][state[1].to_sym] == "1"
-        states_selected << state[1]
+      if params[:meeting]
+        if params[:meeting][state[1].to_sym] == "1"
+          states_selected << state[1]
+        end
       end
     end
     states_selected
+  end
+
+  def meeting_params
+    params.require(:meeting).permit(
+      :name ,
+      :fellowship_id ,
+      :location ,
+      :address ,
+      :city ,
+      :state ,
+      :zip ,
+      :format ,
+      :language ,
+      :meeting_time ,
+      :description ,
+      :hadicapped_accessable ,
+    )
   end
 end
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,7 +26,7 @@ class Users::SessionsController < Devise::SessionsController
   private
   def after_sign_in_path_for(resource)
     session[:just_logged_in] = true
-    users_root_path
+    root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
